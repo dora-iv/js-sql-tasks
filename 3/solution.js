a22bc551b9cf9954e44b4810a321ddfaa0b7197b
dora-iv <167539204+dora-iv@users.noreply.github.com>
2024-11-06 14:48:12 +0300

    solution

--- 3/solution.js
+++ 3/solution.js
@@ -1,14 +1,21 @@
 import postgres from "postgres";
 
 const config = {
-  host: "127.0.0.1",
-  user: "postgres",
-  password: "",
-  port: 5432,
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    port: 5432,
 };
 
 export default async (book) => {
-  // BEGIN (write your solution here)
+    // BEGIN (write your solution here)
+    const sql = postgres(config);
 
-  // END
+    const result = await sql`
+        INSERT INTO books (title, author)
+        VALUES (${book.title}, ${book.author})
+    `;
+
+    return book;
+    // END
 };
